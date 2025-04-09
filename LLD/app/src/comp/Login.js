987693e8dc0fd6c945e9b468be0cb68c0b759736
Vijay <rebalvijay930@gmail.com>
2025-04-09 22:23:27 +0530

    adding new feat:

--- LLD/app/src/comp/Login.js
+++ LLD/app/src/comp/Login.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useLayoutEffect, useMemo, useRef, useState } from 'react'
 
 const Login = () => {
+  // useLayoutEffect
+  const boxRef = useRef();
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    const box = boxRef.current;
+    if (box) {
+      const boxWidth = box.getBoundingClientRect().width;
+      setWidth(boxWidth);
+    }
+  }, []);
+
+  const inputRef = useRef(null)  // Create a reference
+  const handleClick = () => {
+    console.log("useRef rendering.....")
+    inputRef.current.focus();  // Focus the input using ref
+  }
+
   return (
-    <div>
-        <h1>Login page</h1>
+    <div className='p-4 text-center'>
+      <div>
+        <div
+        ref={boxRef}
+        style={{ width: '70%', height: '100px', backgroundColor: 'lightblue' }}
+      >
+        Box
+      </div>
+      <p>Box width: {width}px</p>
+      </div>
+
+      <div>
+        <input 
+          ref={inputRef}
+          type='text' 
+          placeholder='Type here....'
+        />
+        <button onClick={handleClick}>Focus Input</button>
+      </div>
     </div>
   )
 }
 
 export default Login
+
--- LLD/app/src/comp/Team.js
+++ LLD/app/src/comp/Team.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
+
+function Button({handleClick}) {
+  console.log("Button renderd")
+  return <button className='p-2 ml-4 bg-red-500 rounded' onClick={handleClick}>Click me</button>
+}
 
 const Team = () => {
+  //const [count, setCount] = useState(0);
+
+  const [count, setCount] = useState(0);
+  const [dark, setDark] = useState(false)
+  
+
+  const increment = useCallback(()=>{
+      setCount((prev)=>prev+1);
+  },[]); // This function is memoized and will not change on re-renders
+
+  // const increment =()=>{
+  //   setCount(count+1)
+  // }
+
+  const theme = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
+  };
+
+  const expensiveValue = useMemo(()=>{
+    console.log("Calculating....")
+    let total = 0;
+    for( let i = 0; i < 1000000000; i++ ){
+      total = total + i;
+    }
+    return total;
+  },[count]);  // Only re-calculates when `count` changes
+
+
   return (
-    <div>
-      <h1>Team Page</h1>
+    <div className='border border-black w-96 m-auto mt-6 p-4'>
+      <div className='font-bold p-5'>useCallback hook Count: {count}</div>
+      <Button handleClick={increment}/>
+
+      <div style={theme} className='w-96 m-auto mt-5 p-3'>
+        <div>useMemo: {expensiveValue}</div>
+        <div className='flex justify-between mt-5'>
+          <button className='bg-red-600 p-2' onClick={()=> setCount((prev)=>prev+1)}>Increment</button>
+          <button className='bg-red-400 p-2' onClick={()=>setDark((prev)=>!prev)}>Toggle Theme</button>
+        </div>
+        
+    </div>
     </div>
   )
 }
